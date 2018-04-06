6080721d798c0acf30873459dd7f12a0627a6815
shengjiasen <shengjiasen@jd.com>
2018-04-06 23:45:12 +0800

    [other] mkdir -p Logs

--- cron.sh
+++ cron.sh
@@ -1,3 +1,4 @@
 cd `dirname $0`
 source venv/bin/activate
+mkdir -p Logs
 (python3 cron_job.py || echo fail >> Logs/crawler.log) && date >> Logs/crawler.log
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ps aux|grep uwsgi | awk '{print $2}'|xargs kill -9 || echo -n
 source venv/bin/activate
+mkdir -p Logs
 uwsgi viewOJ_uwsgi.ini &
 deactivate
